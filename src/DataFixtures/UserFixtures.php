35f42e9a5d8e0dbe09840616d675fdcb3f4b1bb8
pjehan <pierre.jehan@gmail.com>
2025-01-17 10:30:30 +0100

    Création des fixtures de l'entité User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
+
+    public function load(ObjectManager $manager): void
+    {
+        $admin = new User();
+        $admin->setEmail('admin@salutem.com');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setPassword($this->hasher->hashPassword($admin, '1234'));
+        $manager->persist($admin);
+
+        $user = new User();
+        $user->setEmail('contact@acacias.com');
+        // $user->setRoles(['ROLE_USER']);
+        $user->setPassword($this->hasher->hashPassword($user, '1234'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
